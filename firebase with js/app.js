fb3b2dd1e7c59823dfd228fb7c6ee1011f2a0198
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2017-08-13 20:10:39 +0500

    add and render from firebase

--- firebase with js/app.js
+++ firebase with js/app.js
@@ -1,10 +1,30 @@
-var database = firebase.database().ref('/')
+var database = firebase.database().ref('/');
+var input = document.getElementById('input')
+var unOrderList = document.getElementById('unOrderList')
 
-function demo() {
-    var user = {
-        name: 'Haider',
-        num: 234567,
-        age: 23456
+function todo() {
+    var todo = {
+        item: input.value,
+        todo: 'DONE'
     }
-    database.child('batch4').push(user)
+    database.child('todo').push(todo)
 }
+
+database.child('todo').on("child_added", function (snapshot) {
+    var demo = snapshot.val()
+    demo.id = snapshot.key
+
+    var list = document.createElement('LI')
+    var tagText = document.createTextNode(demo.item)
+    list.appendChild(tagText)//<li>getInput</li>
+    unOrderList.appendChild(list)
+})
+
+
+
+
+
+
+
+
+
