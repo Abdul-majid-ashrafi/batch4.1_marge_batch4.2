c5e70516a31f18341b22734cdbea7d148a816f36
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2017-08-26 17:51:02 +0500

    authantication complete

---
+++ Authantication done with firebase/login/login.js
@@ -0,0 +1,34 @@
+var database = firebase.database().ref('/')
+
+var email = document.getElementById('inputEmail')
+var pass = document.getElementById('inputPassword')
+
+document.getElementById('stop')
+    .addEventListener("submit", function (event) {
+        event.preventDefault()
+        var user = {
+            email: email.value,
+            password: pass.value
+        }
+
+        firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+            .then(function (success) {
+                // console.log(success.uid)
+                database.child('users/' + success.uid)
+                 .once('value', function (snapshot) {
+                    console.log(snapshot.val())
+                })
+            })
+            .catch(function (error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                if (errorCode === 'auth/wrong-password') {
+                    alert('Wrong password.');
+                } else {
+                    alert(errorMessage);
+                }
+                console.log(error);
+            });
+
+    })
