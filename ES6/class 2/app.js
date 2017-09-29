17eb2c21180d3238126b90593774eefb38ac0ff0
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2017-09-29 11:56:03 +0500

    start ES6

---
+++ ES6/class 2/app.js
@@ -0,0 +1,95 @@
+// // var funcs = [];
+// // for (var i = 0; i < 10; i++) {
+// //     funcs.push(function () {
+// //         console.log(i);
+// //     });
+// // }
+// // funcs.forEach(function (func) {
+// //     func();
+// //  });
+
+
+// // var funcs = [];
+// // for (var i = 0; i < 10; i++) {
+// //     funcs.push((function (value) {
+// //         return function () {
+// //             console.log(value);
+// //         }
+// //     }(i)));
+// // }
+// // // console.log('value', value)
+// // //    console.log(i)
+// // funcs.forEach(function (func) {
+// //     func(); // outputs 0, then 1, then 2, up to 9
+// // });
+
+
+
+// // setTimeout(function() {
+// // console.log("Out side timer",i);
+// // }, 1000);
+
+// // for (var i = 0; i < 10; i++) {
+// //     // process(items[i]);
+// //     console.log(i);
+// // }
+// // // i is still accessible here
+// // console.log("Out side",i);
+
+
+
+// // var funcs = [];
+// // for (let i = 0; i < 10; i++) {
+// //     funcs.push(function () {
+// //         console.log(i);
+// //     });
+// // }
+// // funcs.forEach(function (func) {
+// //     func();
+// //  });
+
+
+
+// // var funcs = [],
+// //     object = {
+// //         a: true,
+// //         b: true,
+// //         c: true
+// //     };
+// // for (let key in object) {
+// //     funcs.push(function () {
+// //         console.log(key);
+// //     });
+// // }
+// // funcs.forEach(function (func) {
+// //     func(); // outputs "a", then "b", then "c"
+// // });
+
+
+// // const obj = {
+// // name : "demo",
+// // color : "red"
+// // }
+// // Object.freeze(obj)
+// // obj.name = "MAjid"
+
+// var name = "majid";
+// var add = "khi";
+// alert(`hello ${name} ${add} `)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
